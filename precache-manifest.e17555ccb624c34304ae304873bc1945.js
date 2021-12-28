a648a96e7b36815415b42e4d87fb2ac5cf592a8c
Debabrata Sahu/CORP/IT/MNSR <dsahu@mindagroup.com>
2021-12-28 15:57:22 +0530

    Updates

---
+++ precache-manifest.e17555ccb624c34304ae304873bc1945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ad2c7cfd9dbf1628be1",
+    "url": "/TextFormatter/static/css/main.bb921b47.chunk.css"
+  },
+  {
+    "revision": "8ad2c7cfd9dbf1628be1",
+    "url": "/TextFormatter/static/js/main.8ad2c7cf.chunk.js"
+  },
+  {
+    "revision": "e88f3e01182016b2c5ca",
+    "url": "/TextFormatter/static/js/1.e88f3e01.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextFormatter/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "1e9f4f33f3fc514b7daf",
+    "url": "/TextFormatter/static/js/runtime~main.1e9f4f33.js"
+  },
+  {
+    "revision": "cd1a277ea673d79878292ae51216cb4c",
+    "url": "/TextFormatter/index.html"
+  }
+];
